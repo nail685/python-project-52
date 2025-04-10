e8e3c3469f47cf40d2d7eb421e1dad30cd6d6d38
Nail Girfanov <nail.ildarovich@gmail.com>
2025-04-11 01:46:58 +0500

    change

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -50,12 +50,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'task_manager',
-    'django_filters',
-    'django_bootstrap5',
-    'task_manager.labels',
-    'task_manager.statuses',
-    'task_manager.users',
-    'task_manager.tasks',
+    # 'django_filters',
+    # 'django_bootstrap5',
+    # 'task_manager.labels',
+    # 'task_manager.statuses',
+    # 'task_manager.users',
+    # 'task_manager.tasks',
     
 ]
 
