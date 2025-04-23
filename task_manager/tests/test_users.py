1792f49ad72f8d98d938d52952639e3a065e8cfe
Nail Girfanov <nail.ildarovich@gmail.com>
2025-04-24 01:34:42 +0500

    change

--- task_manager/tests/test_users.py
+++ task_manager/tests/test_users.py
@@ -13,9 +13,9 @@ class UserTestCase(TestCase):
         self.assertTemplateUsed(resp, template_name='general/general_form.html')
 
         resp = self.client.post(reverse('user_create'), {
-            'first_name': 'Nikolay',
-            'last_name': 'Melnikov',
-            'username': 'test',
+            'first_name': 'Nail',
+            'last_name': 'Ivanovich',
+            'username': 'fatty',
             'password1': 'Test123@#',
             'password2': 'Test123@#',
         })
@@ -23,9 +23,9 @@ class UserTestCase(TestCase):
         self.assertRedirects(resp, reverse('login'))
 
         user = User.objects.last()
-        self.assertEqual(user.first_name, 'Nikolay')
-        self.assertEqual(user.last_name, 'Melnikov')
-        self.assertEqual(user.username, 'test')
+        self.assertEqual(user.first_name, 'Nail')
+        self.assertEqual(user.last_name, 'Ivanovich')
+        self.assertEqual(user.username, 'fatty')
 
         resp = self.client.get(reverse('users'))
         self.assertTrue(len(resp.context['users']) == 3)
@@ -50,19 +50,19 @@ class UserTestCase(TestCase):
         resp = self.client.post(
             reverse('user_update', kwargs={'pk': user.id}),
             {
-                'first_name': 'Petya',
-                'last_name': 'Piter',
-                'username': 'Petr1',
-                'password1': 'Te1@',
-                'password2': 'Te1@',
+                'first_name': 'Ivan',
+                'last_name': 'Niko',
+                'username': 'Ivashka',
+                'password1': 'Tell1@',
+                'password2': 'Tell1@',
             }
         )
         self.assertEqual(resp.status_code, 302)
         user.refresh_from_db()
-        self.assertEqual(user.first_name, 'Petya')
+        self.assertEqual(user.first_name, 'Ivan')
 
     def test_DeleteUser(self):
-        user = User.objects.get(username="Masha003")
+        user = User.objects.get(username="Gamer")
         resp = self.client.get(
             reverse('user_delete', kwargs={'pk': user.id})
         )
